c029bdbec8af433077dcbf133d084820a6e93d9c
wing771201 <garciacruz771@gmail.com>
2025-02-26 16:59:34 -0700

    update: Mas tiempo de token

--- backendcasei/settings.py
+++ backendcasei/settings.py
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=8),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     # Puedes agregar más configuraciones según tus necesidades
 }
