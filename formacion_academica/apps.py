b261b1264a465d4f3295b3b081b2487b3eebc62f
Sergio Barreras González <sergiobg.isc@gmail.com>
2025-03-14 22:14:03 -0700

    Delete formacion_academica directory

--- formacion_academica/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FormacionAcademicaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'formacion_academica'
--- formacion_academica/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from formacion_academica.models import FormacionAcademica
-
-
-class FormacionAcademicaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FormacionAcademica
-        fields = ['id', 'nivel', 'nombre', 'institucion_pais', 'anio_obtencion', 'cedula_profesional']
