4ec0cdd12d85eab624736ead5b87b608b1a0dec0
wing771201 <garciacruz771@gmail.com>
2025-04-25 14:15:58 -0700

    fix: atributo de egreso

--- atributo_egreso/models.py
+++ atributo_egreso/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class AtributoEgreso(models.Model):
-    siglas = models.CharField(max_length=10, unique=True)
+    siglas = models.CharField(max_length=10, unique=True, null=True, blank=True)
     descripcion = models.TextField()
 
     def __str__(self):
